18276d74a6d83c138c8cac3140882e4d19ce5063
Daniel Kończyk <daniel@konczyk.net>
2016-03-02 18:35:27 +0100

    Refactor quickunion client

--- src/QuickUnionClient.java
+++ src/QuickUnionClient.java
@@ -1,36 +1,11 @@
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParameterException;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class QuickUnionClient {
 
-    private Random rand = new Random();
+    private final Random rand = new Random();
 
-    public static void main(String[] args) {
-        QuickUnionArgs quickUnionArgs = new QuickUnionArgs();
-        JCommander jc = new JCommander(quickUnionArgs);
-        try {
-            jc.parse(args);
-            quickUnionArgs.validate();
-            if (quickUnionArgs.help || args.length == 0) {
-                jc.usage();
-                return;
-            }
-            QuickUnionClient client = new QuickUnionClient();
-            if (!quickUnionArgs.stdin) {
-                client.runRandom(quickUnionArgs.nodes, quickUnionArgs.connections);
-            } else {
-                client.runStdIn();
-            }
-        } catch (ParameterException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    public void runRandom(int nodes, int connections) {
+    private void runFromRandom(int nodes, int connections) {
         QuickUnion qu = new QuickUnion(nodes);
 
         for (int i = 0; i < connections; ++i) {
@@ -46,9 +21,9 @@ public class QuickUnionClient {
         System.out.println("Components: " + qu.count());
     }
 
-    public void runStdIn() {
-        int nodes = 0;
-        int components = 0;
+    private void runFromStdin() {
+        int nodes;
+        int components;
         try (Scanner scanner = new Scanner(System.in)) {
             nodes = scanner.nextInt();
             QuickUnion qu = new QuickUnion(nodes);
@@ -67,52 +42,17 @@ public class QuickUnionClient {
         System.out.println("Components: " + components);
     }
 
-    private static class QuickUnionArgs {
 
-        private static final String NODES = "--nodes";
-        @Parameter(
-            names = {NODES, "-n"},
-            description = "Number of nodes",
-            validateWith = PositiveIntegerValidator.class)
-        private Integer nodes;
+    public static void main(String[] args) {
+        QuickUnionConfig config = QuickUnionConfig.parseConfig(args);
+        QuickUnionClient client = new QuickUnionClient();
 
-        private static final String CONN = "--connections";
-        @Parameter(
-            names = {CONN, "-c"},
-            description = "Number of random connections",
-            validateWith = PositiveIntegerValidator.class)
-        private Integer connections;
-
-        @Parameter(
-            names = {"--help", "-h"},
-            description = "Usage help",
-            help = true)
-        private boolean help = false;
-
-        private static final String STDIN = "--stdin";
-        @Parameter(
-            names = {STDIN, "-"},
-            description = "Read data from stdin "
-                + "(nodes first, followed by connection pairs)")
-        private boolean stdin = false;
-
-        public void validate() throws ParameterException {
-
-            if (stdin && nodes != null) {
-                throw new ParameterException(
-                    "Parameters " + STDIN + " and " + NODES + " are mutually exclusive");
-            } else if (stdin && connections != null) {
-                throw new ParameterException(
-                    "Parameters " + STDIN + " and " + CONN + " are mutually exclusive");
-            } else if (nodes != null && connections == null) {
-                throw new ParameterException(
-                    "Parameter " + CONN + " is required whenever " + NODES + " is used");
-            } else if (nodes == null && connections != null) {
-                throw new ParameterException(
-                    "Parameter " + NODES + " is required whenever " + CONN + " is used");
-            }
+        if (config.isStdin()) {
+            client.runFromStdin();
+        } else {
+            client.runFromRandom(config.getNodes(), config.getConnections());
         }
-
     }
+
 
 }
---
+++ src/QuickUnionConfig.java
@@ -0,0 +1,92 @@
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+
+public class QuickUnionConfig {
+
+    private static final String NODES = "--nodes";
+    private static final String CONN = "--connections";
+    private static final String STDIN = "--stdin";
+
+    @Parameter(
+        names = {NODES, "-n"},
+        description = "Number of nodes",
+        validateWith = PositiveIntegerValidator.class)
+    private int nodes;
+
+    @Parameter(
+        names = {CONN, "-c"},
+        description = "Number of random connections",
+        validateWith = PositiveIntegerValidator.class)
+    private int connections;
+
+    @Parameter(
+        names = {"--help", "-h"},
+        description = "Usage help",
+        help = true)
+    private boolean help = false;
+
+    @Parameter(
+        names = {STDIN, "-"},
+        description = "Read data from stdin "
+            + "(nodes first, followed by connection pairs)")
+    private boolean stdin = false;
+
+    public int getNodes() {
+        return nodes;
+    }
+
+    public int getConnections() {
+        return connections;
+    }
+
+    public boolean isStdin() {
+        return stdin;
+    }
+
+    public static QuickUnionConfig parseConfig(String[] args) {
+        QuickUnionConfig config = new QuickUnionConfig();
+        JCommander jc = new JCommander(config);
+        jc.setProgramName("QuickUnionClient");
+        if (args.length == 0) {
+            jc.usage();
+            System.exit(0);
+        }
+
+        try {
+            jc.parse(args);
+            config.validate();
+        } catch (ParameterException | IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        if (config.help) {
+            jc.usage();
+            System.exit(0);
+        }
+
+        return config;
+    }
+
+    private void validate() {
+
+        if (stdin && nodes > 0) {
+            throw new ParameterException(
+                "Parameters " + STDIN + " and " + NODES +
+                " are mutually exclusive");
+        } else if (stdin && connections > 0) {
+            throw new ParameterException(
+                "Parameters " + STDIN + " and " + CONN +
+                " are mutually exclusive");
+        } else if (nodes > 0 && connections == 0) {
+            throw new ParameterException(
+                "Parameter " + CONN + " is required whenever " +
+                NODES + " is used");
+        } else if (nodes == 0 && connections > 0) {
+            throw new ParameterException(
+                "Parameter " + NODES + " is required whenever " +
+                CONN + " is used");
+        }
+    }
+}
